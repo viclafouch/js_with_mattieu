b68d7828091175dc90908a951f646036b0f3602f
viclafouch <victor.dlf@outlook.fr>
2020-10-19 23:27:22 +0200

    add exo 4

---
+++ exercice4.js
@@ -0,0 +1,6 @@
+// Exercice 4
+/**
+Créer un timer.
+Toute les secondes, incrémenter un chiffre pour le faire afficher dans le span#timer/
+Un bouton doit permettre de recommencer et un autre bouton pour faire pause/play
+*/
