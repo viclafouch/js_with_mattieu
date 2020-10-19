e0ad0e1d1d3a2879706fef73afd719cd0041d3d8
viclafouch <victor.dlf@outlook.fr>
2020-10-19 23:52:24 +0200

    add exercice 5

--- exercice4.js
+++ exercice4.js
@@ -4,3 +4,5 @@ Créer un timer.
 Toute les secondes, incrémenter un chiffre pour le faire afficher dans le span#timer/
 Un bouton doit permettre de recommencer et un autre bouton pour faire pause/play
 */
+
+console.log('exercice 4')
---
+++ exercice5.js
@@ -0,0 +1,12 @@
+/**
+Créer une liste de posts.
+Le formulaire permet de récupérer des données et dynamiquement ajouter l'article dans la liste de droite.
+2 options possibles:
+- Soit boucler sur un tableau de posts à chaque fois qu'un post est ajouté afin de recréer la liste en entier.
+OU
+- Ajouter l'article créé en fin de liste.
+
+BONUS : Utiliser le localStorage afin de faire persister le tableau des articles (si on actualise la page, on a toujours nos articles). Attention, que de nouvelles choses dans ce bonus.
+ */
+
+console.log('exercice 5')
