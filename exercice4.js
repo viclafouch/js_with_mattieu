e4813d7e64dae666c06d240668e2468ce216a307
mmdwc <intramoros@gmail.com>
2020-10-23 19:15:44 +0100

    update cours 3

--- exercice4.js
+++ exercice4.js
@@ -6,3 +6,57 @@ Un bouton doit permettre de recommencer et un autre bouton pour faire pause/play
 */
 
 console.log('exercice 4')
+
+const timerElm = document.getElementById('timer')
+const replayElm = document.getElementById('replay')
+const pauseElm = document.getElementById('toggle-timer')
+
+let status = 'IS_PLAYING'
+let currentInterval = null
+let currentTime = null
+let number = 0
+
+function playTimer() {
+  currentInterval = setInterval(() => {
+    if(number === 5) {
+        stopAndReset()
+    }else {
+        number = number + 1
+        timerElm.textContent = number
+    }
+
+
+  }, 1000)
+  pauseElm.textContent = 'Pause'
+  status = 'IS_PLAYING'
+}
+
+function clearTimer() {
+  clearInterval(currentInterval)
+  currentInterval = null
+  pauseElm.textContent = 'Play'
+  status = 'IS_PAUSED'
+}
+
+function stopAndReset() {
+    number = 0
+    timerElm.textContent = number
+    clearTimer()
+}
+
+playTimer(number)
+
+pauseElm.addEventListener('click', function() {
+  if (status === 'IS_PLAYING') {
+    clearTimer()
+  } else {
+    playTimer()
+  }
+})
+
+replayElm.addEventListener('click', function(){
+    stopAndReset()
+    playTimer()
+})
+
+
--- exercice5.js
+++ exercice5.js
@@ -10,3 +10,57 @@ BONUS : Utiliser le localStorage afin de faire persister le tableau des articles
  */
 
 console.log('exercice 5')
+const formElem = document.getElementById('form-post')
+const titleElem = document.getElementById('title')
+const descriptionElem = document.getElementById('description')
+const authorElem = document.querySelector('[type="hidden"]')
+const postListElem = document.querySelector('.post-list')
+// let posts = []
+// if (localStorage.getItem("posts")) {
+//   posts = JSON.parse(localStorage.getItem("posts"))
+// }
+const posts = JSON.parse(localStorage.getItem("posts") || '[]')
+formElem.reset()
+
+for (const post of posts) {
+  createPost(post.title, post.description, post.author)
+}
+
+function createPost(param1, param2, param3) {
+  const li = document.createElement('li')
+  const h1 = document.createElement('h1')
+  const pDescription = document.createElement('p')
+  const pAuthor = document.createElement('p')
+  li.classList.add('post-listitem')
+  h1.classList.add('post-title')
+  pDescription.classList.add('post-description')
+  pAuthor.classList.add('post-author')
+  li.appendChild(h1)
+  li.appendChild(pDescription)
+  li.appendChild(pAuthor)
+  h1.textContent = param1
+  pDescription.textContent = param2
+  pAuthor.textContent = param3
+  postListElem.appendChild(li)
+}
+
+function savePost(object1) {
+  posts.push(object1)
+  localStorage.setItem("posts", JSON.stringify(posts))
+}
+
+formElem.addEventListener('submit', function(e) {
+  e.preventDefault()
+  const title = titleElem.value
+  const description = descriptionElem.value
+  const author = authorElem.value
+  createPost(title, description, author)
+  formElem.reset()
+  const currentPost = {
+      title,
+      description,
+      author,
+  }
+  savePost(currentPost)
+})
+
