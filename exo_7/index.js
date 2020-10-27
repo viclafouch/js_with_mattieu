194fbdb933654069861eeb800b5e6f05fb7b7993
viclafouch <victor.dlf@outlook.fr>
2020-10-27 20:22:52 +0100

    add exos 7 8

---
+++ exo_7/index.js
@@ -0,0 +1,8 @@
+/**
+   * Énoncé :
+    Ecrire un script composé des fonctions suivante :
+
+    Une fonction qui remplit un tableau par des noms  saisis par l'utilisateur  via une boite de dialogue , (tu peux utiliser prompt())
+    Une fonction qui renvoie au hasard le nom gagnant et qui sera lancée lorsque l'utilisateur clique sur le  bouton "tirage au sort" .
+    Indication : Utiliser la fonction Math.random() pour tirer un numéro au hasard
+ */
---
+++ exo_8/index.js
@@ -0,0 +1,11 @@
+/**
+   * Énoncé :
+    Récupérer les posts via le point d'API en utilisant fetch (check doc)
+    Afficher la liste des posts dans le html (title / description suffit).
+    Un champs doit permettre de filtrer le titre (en utilisant display none/block) en direct. J'écris et la liste doit n'afficher que ce que je recherche.
+
+    BONUS : La recherche est persistante (via localStorage)
+
+ */
+
+const API_POSTS = 'https://jsonplaceholder.typicode.com/posts'
